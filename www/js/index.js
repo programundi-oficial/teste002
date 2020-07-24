56afc0c27f831cf10cc97840c874780f281333a2
programundi-oficial <rodrigo@sicapsolucoes.com.br>
2020-07-24 19:37:02 -0300

    001

---
+++ www/js/index.js
@@ -0,0 +1,35 @@
+
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+		cordova.plugins.backgroundMode.enable();
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }
+};
+
+function ativar_back(){
+	cordova.plugins.backgroundMode.enable();	
+}
+
+function vericar_ativo(){
+	alert(cordova.plugins.backgroundMode.isActive());
+}
