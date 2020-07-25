423600eb65890c3bf644987a9ebdb7696e3c6782
programundi-oficial <rodrigo@sicapsolucoes.com.br>
2020-07-25 10:21:56 -0300

    1234787

--- www/js/index.js
+++ www/js/index.js
@@ -1,36 +1,30 @@
+
 var app = {
     // Application Constructor
     initialize: function() {
         this.bindEvents();
     },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
+    
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
+    
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
-        app.pluginInitialize();
-        socket.init();
+		app.pluginInitialize();
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+        var parentElement 		= document.getElementById(id);
+        var listeningElement 	= parentElement.querySelector('.listening');
+        var receivedElement 	= parentElement.querySelector('.received');
 
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
     },
-    // Initialize plugin
+	// Initialize plugin
     pluginInitialize: function() {
         var silentBtn = document.getElementById('silent'),
             modeBtn   = document.getElementById('mode'),
@@ -51,116 +45,13 @@ var app = {
         } else {
             app.onSilentButtonClicked();
         }
-    },
-    // Toggle the silent mode
-    onSilentButtonClicked: function() {
-        var plugin   = cordova.plugins.backgroundMode,
-            btn      = document.getElementById('silent'),
-            isSilent = !plugin.getDefaults().silent;
-
-        app.setButtonClass(btn, isSilent);
-        plugin.setDefaults({ silent: isSilent });
-    },
-    // Enable or disable the backgroud mode
-    onModeButtonClicked: function() {
-        var plugin = cordova.plugins.backgroundMode;
-        plugin.setEnabled(!plugin.isEnabled());
-    },
-    // Update CSS classes
-    onModeEnabled: function() {
-        var btn = document.getElementById('mode');
-        app.setButtonClass(btn, true);
-        cordova.plugins.notification.badge.registerPermission();
-    },
-    // Update CSS classes
-    onModeDisabled: function() {
-        var btn = document.getElementById('mode');
-        app.setButtonClass(btn, false);
-    },
-    // Toggle 'active' CSS class and return new status
-    setButtonClass: function(el, setActive) {
-        if (setActive) {
-            el.className += ' active';
-        } else {
-            el.className = el.className.replace(' active', '');
-        }
-    },
-    // To update the badge in intervals
-    timer: null,
-    // Update badge once mode gets activated
-    onModeActivated: function() {
-        var counter = 0;
-
-        plugin.disableWebViewOptimizations();
-
-        app.timer = setInterval(function () {
-            counter++;
-
-            console.log('Running since ' + counter + ' sec');
-            socket.doSend('Running since ' + counter + ' sec');
-
-            cordova.plugins.notification.badge.set(counter);
-
-            if (counter % 15 === 0) {
-                cordova.plugins.backgroundMode.configure({
-                    text: 'Running since ' + counter + ' sec'
-                });
-
-                if (navigator.vibrate) {
-                    navigator.vibrate(1000);
-                }
-            }
-        }, 1000);
-    },
-    // Reset badge once deactivated
-    onModeDeactivated: function() {
-        cordova.plugins.notification.badge.clear();
-        clearInterval(app.timer);
     }
 };
 
-var socket = {
-    _socket: null,
-
-    init: function() {
-        if (!window.hasOwnProperty('WebSocket'))
-            return;
-
-        this._socket = new WebSocket('ws://echo.websocket.org/');
-
-        this._socket.onopen    = function(evt) { socket.onOpen(evt); };
-        this._socket.onclose   = function(evt) { socket.onClose(evt); };
-        this._socket.onmessage = function(evt) { socket.onMessage(evt); };
-        this._socket.onerror   = function(evt) { socket.onError(evt); };
-    },
-
-    onOpen: function(evt) {
-        console.log('CONNECTED');
-        this.doSend('background-mode plugin rocks');
-    },
-
-    onClose: function(evt) {
-        console.log('DISCONNECTED');
-    },
-
-    onMessage: function(evt) {
-        console.log('RECEIVED: ' + evt.data);
-    },
-
-    onError: function(evt) {
-        console.log('ERROR: ' + evt.data);
-    },
-
-    doSend: function(message) {
-        if (this._socket) {
-            console.log('SENT: ' + message);
-            this._socket.send(message);
-        }
-    }
-};
-
-if (window.hasOwnProperty('Windows')) {
-    alert = function (msg) { new Windows.UI.Popups.MessageDialog(msg).showAsync(); };
+function ativar_back(){
+	cordova.plugins.backgroundMode.enable();	
 }
 
-app.initialize();
+function vericar_ativo(){
+	alert(cordova.plugins.backgroundMode.isActive());
+}
